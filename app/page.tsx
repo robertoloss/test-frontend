3d6b20a46fd671a6989bbc4542d66b5e9b6dc09b
Roberto Loss <robertoloss@gmail.com>
2025-08-02 12:43:54 -0400

    force dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import TaskList from "@/components/task-list";
 import { serverUrl } from "@/lib/utils";
 import { Suspense } from "react";
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
 
   const response = await fetch(serverUrl + '/api/tasks')
